60d2692f14da1776a3d9b56365b7d05043023642
Beline23 <144127688+Beline23@users.noreply.github.com>
2023-09-26 09:09:56 -0300

    Add files via upload

---
+++ Olá Mundo (Inicio do Vscode).cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Standard Input Output
+// STD - I - O
+//stdio
+
+#include <stdlib.h>
+
+int main () {  // <- Criando uma função principal // int= inteiro/main = principal // Aas chaves indicam o inicio de um bloco de comandos e o fim.
+	printf("Olá, Mundo!"); // Comando para escrever na tela
+	return 0; // <- Ferramenta de controle, para verificar se o código foi bem exacutado, quando o resultado = 0 o programa foi bem executado.
+	}
+
