b89021384b19e401819c31aa1777e9ba3a47314e
Beline23 <144127688+Beline23@users.noreply.github.com>
2023-09-26 11:22:54 -0300

    Add files via upload

---
+++ Solicitando Dados.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Declarando váriaveis.
+
+    int idade;
+    float peso;
+    char sexo;
+
+    // Solicitando dados para o usuário.
+
+    printf("Digite Sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Digite seu peso: ");
+    scanf("%f", &peso);
+
+    fflush(stdin); // é preciso limpar o buffer do cash para executar uma variaves de caracter apos uma variavel de numeros.
+
+    printf("Digite Seu Sexo: ");
+    scanf("%c", &sexo);
+
+    // Exibindo dados do usuário
+
+    system("cls || clear"); // Limpatela  => cls = para window / clear = para linux 
+
+    printf("Idade : %d anos. \n", idade);
+    printf("Peso : %.2f KG. \n", peso);
+    printf("Sexo : %c \n", sexo);
+
+    return 0;
+}
