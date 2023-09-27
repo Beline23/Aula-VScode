c4420d090792860f5a64f3e1d7cd57cb607fc961
Beline23 <144127688+Beline23@users.noreply.github.com>
2023-09-27 11:04:12 -0300

    Add files via upload

---
+++ Exercicio 01 27.09.2023 (Aplicando condicionais).c
@@ -0,0 +1,34 @@
+// Exercicio 01 27.09.2023 (Aplicando condicionais)
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+
+    // Permitindo acentuaÃ§Ã£o:
+
+setlocale:
+    (LC_ALL, "portuguese"); // Indicado deixar o "" vazio sem nenhum valor, pois assim ele verifica o idioma existente no dispositivo utilizado
+    float numero;
+
+    printf("Escreva o número desejado: ");
+    scanf("%f", &numero);
+
+    if (numero > 10) // <= Condicional
+    {
+        printf("É MAIOR QUE 10!");
+    }
+    if (numero = 10)
+    {
+        printf("É IGUAL A 10!");
+    }
+
+    else // <= Se a condiÃ§ao nao se confirmar entao...
+    {
+        printf("NÃO É MAIOR QUE 10!");
+
+    
+        return 0;
+    }
+}
